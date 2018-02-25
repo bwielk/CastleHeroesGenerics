afcf250a72c9836fde606f2906977bd99e9c98f2
bwielk <bwielk@gmail.com>
2018-02-25 19:57:16 +0100

    Barbarian army accepts Barbarian creatures

--- src/league/Army.java
+++ src/league/Army.java
@@ -1,13 +1,17 @@
 package league;
 
-public class Army {
+import java.util.ArrayList;
+
+public class Army<T extends Creature>{
 	
 	private int wins;
 	private int losts;
+	private ArrayList<T> army;
 	
 	public Army(){
 		this.wins = 0;
 		this.losts = 0;
+		this.army = new ArrayList<T>();
 	}
 
 	public int getWins() {
@@ -16,5 +20,17 @@ public class Army {
 
 	public int getLosts() {
 		return losts;
+	}
+
+	public boolean add(T creature){
+		if(!army.contains(creature)){
+			army.add(creature);
+			return true;
+		}
+		return false;
+	}
+	
+	public int getNumOfUnits(){
+		return army.size();
 	}
 }
